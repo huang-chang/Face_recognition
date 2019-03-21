dc8e1d3eb5b9a3325b18bc8b2d6c49fd44332f80
Chang Huang <1185570880@qq.com>
2019-03-20 23:27:54 -0700

    Add files via upload

---
+++ train_classifier.sh
@@ -0,0 +1,1 @@
+python /data/facenet-master/src/classifier.py TRAIN /data/facenet-master/face_12_23_selected/ /data/facenet-master/face_model/tf_freeze_model/face_389.pb face_389_svm.pkl --batch_size 50
---
+++ train_mlp.sh
@@ -0,0 +1,1 @@
+python /data1/facenet-master/src/mlp_train.py TRAIN /data1/facenet-master/face_565_8_24_selected/ /data1/facenet-master/face_model/tf_freeze_model/face_565_2018_8_26.pb --batch_size 50
